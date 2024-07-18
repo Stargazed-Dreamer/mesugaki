1197952f834c5079a7706d485d09bb8ddb1d846a
StaryDreamer <101247328+StaryDreamer@users.noreply.github.com>
2024-07-18 15:52:29 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mesugaki",
+    version="1.0",
+    packages=find_packages(),
+    description="将python异常处理的信息转为雌小鬼和傲娇口气~",
+    url="https://github.com/StaryDreamer/mesugaki",
+    author="StaryDreamer",
+    license="Apache-2.0 license",
+)
